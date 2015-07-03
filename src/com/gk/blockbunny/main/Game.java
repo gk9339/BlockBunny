412d013f3edf8c5e2ae46104b8b0a5ee48200e05
George Kottler <kottlerg@student.tranby.wa.edu.au>
2015-07-03 16:26:45 +0100

    Initial Commit
    Set up main class and window properties

---
+++ src/com/gk/blockbunny/main/Game.java
@@ -0,0 +1,29 @@
+package com.gk.blockbunny.main;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Game implements ApplicationListener{
+	
+	public static final String TITLE = "Block Bunny";
+	public static final int V_WIDTH = 320;
+	public static final int V_HEIGHT = 240;
+	public static final int SCALE = 2;
+	
+	public void create() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	public void dispose() {
+		
+	}
+	
+	
+	public void resize(int w, int h) {}
+	public void pause() {}
+	public void resume() {}
+
+}
